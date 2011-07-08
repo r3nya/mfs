81b6916eea9ed006538d6bf25214c48b13c46fde
Andrew <renya.vad@gmail.com>
2011-07-08 20:06:07 +0400

    added scripts for R

---
+++ new.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Script for R.
+
+#repos
+
+# Samsung-tools
+apt-add-repository ppa:voria
+
+# LibreOffice
+add-apt-repository ppa:libreoffice/ppa
+
+# Chromium stable
+add-apt-repository ppa:chromium-daily/stable
+
+# umplayer+mplayer
+add-apt-repository ppa:webupd8team/umplayer
+add-apt-repository ppa:motumedia/mplayer-daily
+
+# deadbeef
+add-apt-repository ppa:alexey-smirnov/deadbeef
+
+# ubuntu-tweak
+add-apt-repository ppa:tualatrix/ppa
+
+add-apt-repository "deb http://linux.dropbox.com/ubuntu $(lsb_release -cs) #main" && wget -q -O- http://ubuntu-tweak.com/source/dropbox/key | apt-key add -
+
+apt-key adv --keyserver pgp.mit.edu --recv-keys 0xd66b746e
+wget --output-document=/etc/apt/sources.list.d/medibuntu.list http://www.medibuntu.org/sources.list.d/$(lsb_release -cs).list && \
+apt-get --quiet update && apt-get --yes --quiet --allow-unauthenticated install medibuntu-keyring
+
+#Installing software
+apt-get --yes --quiet --allow-unauthenticated install `cat ./R_list.txt | sed 's/^\#.*//g' |tr -s "[\n]" | sed -e :a -e '$!N;s/\n/\ /;ta'`
+
+#Upgrade
+apt-get upgrade
